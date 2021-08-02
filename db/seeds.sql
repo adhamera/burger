4c0051134bb897e72c4c681b016034fcd1568e65
Anusha <adhamera@gmail.com>
2021-08-01 19:16:46 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Veggie');
+INSERT INTO burgers (name) VALUES ('Impossible');
+INSERT INTO burgers (name, devoured) VALUES ('Chicken', true);
+INSERT INTO burgers (name, devoured) VALUES ('Portobello', true);
+INSERT INTO burgers (name, devoured) VALUES ('Ham', true);
+INSERT INTO burgers (name) VALUES ('Fish');
